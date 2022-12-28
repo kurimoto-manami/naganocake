0e1e955f1e6c0bc4216f2cdee208ed5d64a3fd91
ManamiKurimoto <114746043+kurimoto-manami@users.noreply.github.com>
2022-12-28 12:17:52 +0000

    public cartitem

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,18 +1,40 @@
 class Public::OrdersController < ApplicationController
 
   def new
+    @order = Order.new
+  end
+  
+  def create
+    @item = Item.find(params[:item_id])
+    @order = @item.order.new(order_params)
+    @order.save
+    redirect_to items_path
   end
   
   def index
+    @orders = Order.find(params[:id])
   end
   
   def show
+    @orders = Order.find(params[:id])
   end
   
   def confirm
+    @item = Item.find(params[:item_id])
+    @order = @item.order.new(order_params)
   end
   
   def completion
   end
   
+  
+  private
+
+  def order_params
+    params.require(:order).permit(:sipping_cost, :total_payment, :payment_method)
+  end
+
+  def address_params
+    params.require(:order).permit(:name, :address)
+  end
 end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -3,7 +3,7 @@ class CartItem < ApplicationRecord
   belongs_to :item
   belongs_to :customer
   
-  def sum_of_price
-    product.price * quantity
+  def sum_o_price
+    item.taxin_price * quantity
   end
 end
